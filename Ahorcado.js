1f1cd1beb10df35dcda25fe496104768e532bbfb
Jonathan Sarli <jonathan.sarli@correo.unimet.edu.ve>
2024-05-24 00:21:25 -0700

    Desarrollo Archivo java. Función para generar numero random y poder agarrar palabra del array

--- Ahorcado.js
+++ Ahorcado.js
@@ -1,1 +1,34 @@
-"Prueba"
+
+const palabras = [
+    "hipopótamo",
+    "paralelepípedo",
+    "onomatopeya",
+    "sesquipedal",
+    "esternocleidomastoideo",
+    "electroencefalografista",
+    "quiropráctico",
+    "desoxirribonucleico",
+    "anticonstitucionalmente",
+    "ultramicroscópicamente"
+];
+
+const button = document.getElementById("jugar");
+button.addEventListener('Click', iniciar);
+
+
+console.log(button);
+
+function obtener_random(num_min, num_max){
+    const amplitud_valores = num_max - num_min;
+    const valor_al_azar = Math.floor(Math.random()*amplitud_valores) + num_min;
+    return valor_al_azar;
+}
+
+function iniciar(event){
+    const valor_mas_bajo = 0
+    const cant_palabras = palabras.length;
+    const valor_al_azar = obtener_random(0, cant_palabras)
+    
+    const palabra_de_lista = palabras[valor_al_azar];
+    console.log("Hiciste Click");
+}
