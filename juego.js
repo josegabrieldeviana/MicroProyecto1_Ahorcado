72f0db7c3ea4af650c5e38e5e1f2ae95f839ccef
jose <josegabrieldeviana@gmail.com>
2024-05-23 21:48:15 -0400

    empezado el guión

---
+++ juego.js
@@ -0,0 +1,18 @@
+const
+/**!
+ * Primera enseñanza de js del dia si vengo al código y digo que
+ * 
+ * btn.addEventListener, escuchadores de eventos como el drag, click, unload, aquí se añade el soporte para estas funciones  
+ * 
+ * En el escuchador de eventos, event listener, le vamos a poner un click, y si tenemos un nombre de una cfunción lo ponemos directamente al add event listener
+ * Si no esta presente, entonces ponemos function() y ponemos el código o ()=>
+ * 
+ * Si yo hago esto entonces ponemos el console.log
+ * 
+ * El add event listener si no puede leer propiedades del null, es que no existe. Lo que 
+ * Si les aparece el null significa que eso no existe o que eso que hicimos mal no esta. Ahora vamos a decir que este juego
+ * 
+ * Este juego lo creo mínimo
+ * 
+ *  
+ */
